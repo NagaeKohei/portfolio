4419c4759f3d1a72a28cd99d943e18bf72a31437
NagaeKohei <125794854+NagaeKohei@users.noreply.github.com>
2023-06-13 11:09:56 +0900

    Create tophiddenbutton.js

---
+++ URL/housemaker/js/tophiddenbutton.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  var target = $('.target'); // 対象の要素
+
+  // スクロール時の処理
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop(); // スクロール量
+    if(scroll > 700){
+      // 対象の要素が画面内に入った時にフェードインする
+      target.fadeIn();
+    } else {
+      // 対象の要素が画面外に出た時に再び非表示にする
+      target.fadeOut();
+    }
+  });
+});
